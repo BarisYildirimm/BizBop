eb48b46d2745f51f533f674290f333af7ee97052
BarisYildirim00 <b.yildirimbladeco@gmail.com>
2023-10-26 00:32:58 +0300

    Login Screen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import FormContainer from "../components/FormContainer";
 
 const LoginScreen = () => {
-  return <div>LoginScreen</div>;
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      <Form onSubmit={""}>
+        <Form.Group className="my-2" controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={""}
+            onChange={(e) => ""}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className="my-2" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={""}
+            onChange={(e) => ""}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button disabled={""} type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          New User? <Link to={""}>Register</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
 };
 
 export default LoginScreen;
